6b8092966703a6e40dbb5e884dd6acea27f4503a
Warren1212-github <71634410+Warren1212-github@users.noreply.github.com>
2020-11-07 18:00:33 +0800

    有效代码
    感谢大佬帮助，apriori_plus代码是可以运行的，后缀分别为针对不同的数据集进行的关联分析计算

--- test/apriori3.py
+++ test/apriori3.py
@@ -1,8 +1,18 @@
 from numpy import *
  
 # 构造数据
-def loadDataSet():
-    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+#def loadDataSet():
+#    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+def readdata(path):
+    file=open(path,mode='r',encoding='UTF-8')
+    dataMatrix=[]
+    contents = file.readlines()
+    for msg in contents:
+        msg = msg.strip('\n')
+        adm = msg.split('~')
+        dataMatrix.append(adm)
+    file.close()        
+    return dataMatrix
  
 # 将所有元素转换为frozenset型字典，存放到列表中
 def createC1(dataSet):
@@ -66,8 +76,60 @@ def apriori(dataSet, minSupport = 0.5):
         k += 1  # 每次新组合的元素都只增加了一个，所以k也+1（k表示元素个数）
     return L, supportData
  
-dataSet = loadDataSet()
-L,suppData = apriori(dataSet)
-print(L)
-print(suppData)
+#dataSet = readdata('D:\\各种数据集\\专利数据\\citytest.txt')
+#L,suppData = apriori(dataSet)
+#print(L)
+#print(suppData)
+
+
+
+
+
+# 获取关联规则的封装函数
+def generateRules(L, supportData, minConf=0.7):  # supportData 是一个字典
+    bigRuleList = []
+    for i in range(1, len(L)):  # 从为2个元素的集合开始
+        for freqSet in L[i]:
+            # 只包含单个元素的集合列表
+            H1 = [frozenset([item]) for item in freqSet]    # frozenset({2, 3}) 转换为 [frozenset({2}), frozenset({3})]
+            # 如果集合元素大于2个，则需要处理才能获得规则
+            if (i > 1):
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf) # 集合元素 集合拆分后的列表 。。。
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+ 
+# 对规则进行评估 获得满足最小可信度的关联规则
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []  # 创建一个新的列表去返回
+    for conseq in H:
+        conf = supportData[freqSet]/supportData[freqSet-conseq]  # 计算置信度
+        if conf >= minConf:
+            print(freqSet-conseq,'-->',conseq,'conf:',conf)
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+ 
+# 生成候选规则集合
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)): # 尝试进一步合并
+        Hmp1 = aprioriGen(H, m+1) # 将单个集合元素两两合并
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 1):    #need at least two sets to merge
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+ 
+dataSet = readdata('D:\\各种数据集\\专利数据\\city文本.txt')
+L,suppData = apriori(dataSet,minSupport=0.5)
+rules = generateRules(L,suppData,minConf=0.7)
+#rules = generateRules(L,suppData,minConf=0.5)
+print(rules)
+
+
+
+
+
+
+
+
 
